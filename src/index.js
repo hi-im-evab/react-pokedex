74a459ed9088c3b3288a7a4e42674ba02a1a53dc
hi-im-evab <navesregge@gmail.com>
2019-05-23 20:08:25 -0500

    Notes from Feb: 2hrs work started with tictactoe tut to refresh memory got bored with tut, tried axios get retrieved info from axios, displayed pikachu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
 class YourComponent extends React.Component {
     constructor(props) {
@@ -68,16 +68,13 @@ class YourComponent extends React.Component {
     }
 }
 
-
-
 ReactDOM.render(
     // <Game />,
     <YourComponent />,
     document.getElementById('root')
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
