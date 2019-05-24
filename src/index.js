4058be32377260a6bc7587aa5ccf5fc528b8d0c5
hi-im-evab <navesregge@gmail.com>
2019-05-23 23:00:14 -0500

    Reverting back to old async render attempt, keeping Suspense n stuff in mind. Ran into bizarre error. Gonna try something else.

--- src/index.js
+++ src/index.js
@@ -3,66 +3,67 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// class YourComponent extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             data: null
-//         }
-//     }
+class YourComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        }
+    }
 
-//     componentDidMount() {
-//         const axios = require('axios');
+    componentDidMount() {
+        const axios = require('axios');
 
-//         var pokeboy;
+        var pokeboy;
 
-//         const that = this
-//         const setState = this.setState.bind(this);
+        const that = this
+        const setState = this.setState.bind(this);
 
-//         console.log('pre async');
+        console.log('pre async');
 
-//         // return EVERYTHING
-//         console.log('before axios');
-//         for (var i = 1; i <= 20; i++) {
-//             axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon/" + i)
-//                 .then(function (response) {
-//                     console.log(response);
-//                 })
-//                 .catch((error) => {
-//                     console.log(error);
-//                 });
-//         }
-//         console.log('after axios');
+        // return EVERYTHING
+        console.log('before axios');
+        for (var i = 1; i <= 20; i++) {
+            axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon/" + i)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        console.log('after axios');
 
-//     }
+    }
 
-//     render() {
-//         return (
-//             <div>
-//                 POKEMON INFO
+    render() {
+        return (
+            <div>
+                POKEMON INFO
     
-//             {this.state.data === null ?
-//                     <div>Loading</div>
-//                     :
-//                     <div>
+            {this.state.data === null ?
+                    <div>Loading</div>
+                    :
+                    <div>
 
-//                         {this.state.data[0]} <br></br>
+                        {this.state.data[0]} <br></br>
 
 
-//                         <img src={this.state.data[1]}></img> <br></br>
-//                         {this.state.data[2]} <br></br>
-//                         {this.state.data[3]} <br></br>
+                        <img src={this.state.data[1]}></img> <br></br>
+                        {this.state.data[2]} <br></br>
+                        {this.state.data[3]} <br></br>
 
-//                     </div>
-//                 }
-//             </div>
+                    </div>
+                }
+            </div>
 
-//         );
-//     }
-// }
+        );
+    }
+}
 
 ReactDOM.render(
-    <App />,
+    // <App />,
+    <YourComponent />,
     document.getElementById('root')
 );
 
