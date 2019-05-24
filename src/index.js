1b1d4d97cdce69f2f46aa0b6bdcd66e8ba0d5583
hi-im-evab <navesregge@gmail.com>
2019-05-23 20:06:33 -0500

    Added old code from previous tut work circa Feb 2019

--- src/index.js
+++ src/index.js
@@ -4,7 +4,78 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class YourComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        }
+    }
+
+    componentDidMount() {
+        const axios = require('axios');
+
+        var pokeboy;
+
+        const that = this
+        const setState = this.setState.bind(this);
+
+        console.log('pre async');
+
+        axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?name=Pikachu")
+            .then(function (response) {
+                // handle success
+                that.setState();
+                console.log('async');
+
+                console.log(response);
+
+                pokeboy = response.data.data[0];
+
+                //that.setState({pokeboy: pokeboy});
+
+                console.log(pokeboy.id);
+                console.log(pokeboy.image);
+                console.log(pokeboy.name);
+                console.log(pokeboy.types[0]);
+
+                var array = [pokeboy.id, pokeboy.image, pokeboy.name, pokeboy.types[0]];
+
+                setState({ data: array });
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+
+    }
+
+    render() {
+        return (<div>
+            POKEMON INFO
+            {this.state.data === null ?
+                <div>Loading</div>
+                :
+                <div>
+                    {this.state.data[0]} <br></br>
+                    <img src={this.state.data[1]}></img> <br></br>
+                    {this.state.data[2]} <br></br>
+                    {this.state.data[3]} <br></br>
+
+                </div>
+            }
+        </div>);
+    }
+}
+
+
+
+ReactDOM.render(
+    // <Game />,
+    <YourComponent />,
+    document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
