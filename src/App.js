2dc58d19c5cf56422a3f0ec1cafc31dcdc53ef7b
hi-im-evab <navesregge@gmail.com>
2019-05-31 02:17:35 -0500

    Added loadDetails() Details have been converted from JSON to an array Displays only in console log at the moment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
+import './loadDetails';
 
 class App extends Component {
     constructor(props) {
@@ -12,10 +13,79 @@ class App extends Component {
         }
         this.pageFwd = this.pageFwd.bind(this);
         this.pageRev = this.pageRev.bind(this);
+        this.selectPokemon = null;
     }
 
+    /**
+     * Main pokemon loading function.
+     * Runs an axios request for current page number
+     * The 15 pokemons' info is then converted from
+     * JSON to an array that can be used by React's
+     * Array.map function.
+     */
+    loadPokemon() {
+        const setState = this.setState.bind(this);
+        const axios = require('axios');
+
+        // Pokemon objects put into this array
+        var array = [];
+
+        // Axios request for current page number
+        axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.page)
+            .then(function (response) {
+                // convert from JSON response to a usable array
+                // for loop iterates through each pokemon in response
+                for (var i = 0; i < response.data.data.length; i++) {
+                    var pokemon = response.data.data[i];
+                    var types = [];
+
+                    // get pokemon's types
+                    for (var index in pokemon.types) {
+                        types.push(pokemon.types[index]);
+                    }
+
+                    //  push all attributes and types to array
+                    array.push([pokemon.id, pokemon.image, pokemon.name, types]);
+
+                }
+                setState({ data: array });
+            });
+    }
+
+    loadDetails(id) {
+        const setState = this.setState.bind(this);
+        const axios = require('axios');
+
+        var array = [];
+
+        axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon/" + id)
+            .then(function (response) {
+                var pokemon = response.data.data;
+
+                for (var i in pokemon) {
+                    if (i === 'stats') {
+                        var stats = ['stats'];
+                        for (var x in pokemon[i]) {
+                            stats.push( [x, pokemon[i][x]]);
+                        }
+
+                        array.push(stats);
+
+                    } else {
+                        array.push([i, pokemon[i]]);
+                    }
+                }
+
+                console.log(array);
+                //setState({ selectPokemon: array });
+            });
+    }
+
+    /**
+     * Moves page forward 1
+     * if not on last page
+     */
     pageFwd() {
-        // Advance 1 page if not on last page
         if (this.state.page < this.state.endPage) {
             this.setState({ page: this.state.page + 1 },
                 () => {
@@ -24,41 +94,17 @@ class App extends Component {
         }
     }
 
+    /**
+     * Moves page back 1
+     * if not on first page
+     */
     pageRev() {
-        // Go back 1 page if not on first page
         if (this.state.page > this.state.startPage) {
             this.setState({ page: this.state.page - 1 },
                 () => {
                     this.loadPokemon();
                 });
         }
-    }
-
-    loadDetails() {
-        alert('hoy');
-    }
-
-    // Load in Pokemon
-    // range based on ID and page number
-    // ex) pg 3 shows IDs 31-45
-    loadPokemon() {
-        const axios = require('axios');
-
-        var array = [];
-
-        const setState = this.setState.bind(this);
-
-        axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.page)
-            .then(function (response) {
-                for (var i = 0; i < 15; i++) {
-                    var pokeboy = response.data.data[i];
-
-                    // get select attributes from data
-                    array.push([pokeboy.id, pokeboy.image, pokeboy.name]);
-                }
-
-                setState({ data: array });
-            })
     }
 
     componentDidMount() {
@@ -69,10 +115,22 @@ class App extends Component {
         return (
             <div>
                 {data.map(item => (
-                    <div className="pokemon" onClick={this.loadDetails} key={item[0]}>
+                    <div className="pokemon" onClick={(e) => this.loadDetails(item[0])} key={item[0]}>
                         {item[0]}
                         <img src={item[1]}></img>
-                        {item[2]} 
+                        {item[2]}
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+    renderDetails = selectPokemon => {
+        return (
+            <div>
+                {selectPokemon.map(item => (
+                    <div className="pokemon_detail" key={item}>
+                        {item[0]}{item[1]}
                     </div>
                 ))}
             </div>
@@ -81,6 +139,10 @@ class App extends Component {
 
     render() {
         const { data } = this.state;
+
+        if (this.state.selectPokemon != null) {
+            var selectPokemon = this.state.selectPokemon;
+        }
 
         return (
             <div>
@@ -99,6 +161,9 @@ class App extends Component {
                     <div>Loading</div>
                     :
                     <div className="pokemon-list">
+                        {/* RENDER LIST CALLED TWICE ON PAGE CHANGE
+                            TODO: Block first render on state change?
+                        */}
                         {this.renderList(data)}
                     </div>
                 }
@@ -111,6 +176,16 @@ class App extends Component {
                         Page Forward
                     </button>
                     {this.state.page}
+                </div>
+
+                <br></br>
+
+                <div>
+                    {
+                        selectPokemon != null ? (
+                            this.renderDetails(selectPokemon)
+                        ) : null
+                    }
                 </div>
             </div>
         );
---
+++ src/loadDetails.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class LoadDetails extends Component {
+    // TODO: Move loadDetails() n such here later for cleanliness?
+}
+
+export default LoadDetails;
